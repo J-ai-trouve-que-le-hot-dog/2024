98ddbb61814d8a478d19bfc7166b367628d9c3ad
Pierre Chambart <pierre.chambart@ocamlpro.com>
2024-06-28 15:39:08 +0200

    Better print on communicate

--- bin/communicate.ml
+++ bin/communicate.ml
@@ -5,5 +5,9 @@ let () =
   let body = "S" ^ Ast.Encoded_string.(to_raw_string (from_string body)) in
   let result = Api.communicate body in
   let msg = Ast.parse_input result in
-  Format.printf "@.%a@."
-    Ast.pp_expr msg
+  match msg with
+  | Ast.String s ->
+    Format.printf "@.%s@." (Ast.Encoded_string.to_string s)
+  | _ ->
+    Format.printf "@.%a@."
+      Ast.pp_expr msg
