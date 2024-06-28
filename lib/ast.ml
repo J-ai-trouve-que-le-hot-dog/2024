6b171e35270bb9972deda72c3fe2cc2dad48b777
Pierre Chambart <pierre.chambart@ocamlpro.com>
2024-06-28 15:21:35 +0200

    Move decode to Ast

--- lib/ast.ml
+++ lib/ast.ml
@@ -9,12 +9,14 @@ type expr =
   | If of { cond : expr; tbranch : expr; fbranch : expr }  (** If [?] *)
   | Lambda of { var : int; body : expr }  (** Lambda abstractions [L] *)
   | Var of int
+[@@deriving(show)]
 
 and unop =
   | Neg  (** [-] *)
   | Not  (** [!] *)
   | String_to_int  (** [#] *)
   | Int_to_string  (** [$] *)
+[@@deriving(show)]
 
 and binop =
   | Add  (** [+] *)
@@ -31,6 +33,7 @@ and binop =
   | Take  (** [T] *)
   | Drop  (** [D] *)
   | Apply  (** [$] *)
+[@@deriving(show)]
 
 let chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&\'()*+,-./:;<=>?@[\\]^_`|~ \n"
 
@@ -98,4 +101,53 @@ let encode_binop : binop -> char = function
   | Drop -> 'D' 
   | Apply -> '$' 
 
+let rem s = String.sub s 1 (String.length s - 1)
 
+let decode_int s =
+  let r = ref 0 in
+  for i = 0 to String.length s - 1 do
+    r := 94 * !r + Char.code s.[i] - 33
+  done;
+  !r
+
+let rec parse get =
+  let s = get () in
+  match s.[0] with
+  | 'T' -> Bool true
+  | 'F' -> Bool false
+  | 'I' -> Int (decode_int (rem s))
+  | 'S' -> String (decode_string s)
+  | 'U' -> Unop (decode_unop s.[1], parse get)
+  | 'B' ->
+    let e1 = parse get in
+    let e2 = parse get in
+    Binop (decode_binop s.[1], e1, e2)
+  | '?' ->
+    let c = parse get in
+    let e1 = parse get in
+    let e2 = parse get in
+    If { cond = c; tbranch = e1; fbranch = e2 }
+  | 'L' ->
+    let v = decode_int (rem s) in
+    Lambda { var = v; body = parse get }
+  | 'v' ->
+    let v = decode_int (rem s) in
+    Var v
+  | _ -> Bool false
+
+let parse_parts parts =
+  let l = ref parts in
+  let get () =
+    match !l with
+    | [] -> assert false
+    | h :: r -> l := r; h
+  in
+  let acc = ref [] in
+  while !l <> [] do
+    acc := parse get :: !acc;
+  done;
+  List.rev !acc
+
+let parse_input s =
+  let parts = String.split_on_char ' ' s in
+  parse_parts parts
