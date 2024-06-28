c90ee73485ec6540a4a3695e1fc1a1ffc97be47f
Rafaël Bocquet <6364095+RafaelBocquet@users.noreply.github.com>
2024-06-28 15:54:25 +0200

    Fix eval

--- bin/eval.ml
+++ bin/eval.ml
@@ -1,7 +1,7 @@
 
+
 let () =
-  assert(Array.length Sys.argv >= 2);
-  let body = String.concat " " (List.tl (Array.to_list Sys.argv)) in
+  let body = In_channel.input_all stdin in
   let v = Ast.parse_input body in
   let r = Eval.eval EnvEmpty (Eval.term_from_expr v) in
   ()
