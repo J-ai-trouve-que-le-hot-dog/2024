8921130185d336aa4fb02c4918ee7454ebea0d0c
Rafaël Bocquet <6364095+RafaelBocquet@users.noreply.github.com>
2024-06-30 17:55:13 +0200

    layout optimization

--- 3d/layout.ml
+++ 3d/layout.ml
@@ -3,23 +3,43 @@ let component_size component =
   Array.length component ,
   Array.fold_left max 0 (Array.map Array.length component)
 
+let shape : string option array array -> _ = Array.map (Array.map Option.is_some)
+
 let layout_with_sizes n m (components : string option array array list)
   : string option array array option =
+
+  let shapes = CCVector.create () in
+  let shape_count = CCVector.create () in
   let components = Array.of_list components in
-  let ncomponents = Array.length components in
+  
+  Array.iter (fun c ->
+      let i = ref 0 in
+      while !i < CCVector.length shapes && CCVector.get shapes !i <> shape c do
+        incr i
+      done;
+      if !i = CCVector.length shapes then
+        begin
+          CCVector.push shapes (shape c);
+          CCVector.push shape_count 0
+        end;
+      CCVector.set shape_count !i (CCVector.get shape_count !i + 1)
+    )
+    components;
+  
+  let nshapes = CCVector.length shapes in
   let nv = ref 0 in
   let new_var () = incr nv; !nv in
-  let sizes = Array.map component_size components in
+  let sizes = CCVector.map component_size shapes in
   let vars =
-    Array.init ncomponents (fun i ->
-        let (x,y) = sizes.(i) in
+    Array.init nshapes (fun i ->
+        let (x,y) = CCVector.get sizes i in
       Array.init (n+1 - x) (fun _ ->
           Array.init (m+1 - y) (fun _ ->
               new_var ()
             )
         )
     ) in
-  let avars = Array.init ncomponents (fun _ ->
+  let avars = Array.init nshapes (fun _ ->
       CCVector.create ()
     ) in
   let at = Array.init n (fun _ ->
@@ -28,16 +48,16 @@ let layout_with_sizes n m (components : string option array array list)
         )
     )
   in
-  for c = 0 to ncomponents - 1 do
-    let (x,y) = sizes.(c) in
+  for c = 0 to nshapes - 1 do
+    let (x,y) = CCVector.get sizes c in
     for i = 0 to n-x do
       for j = 0 to m-y do
         let v = vars.(c).(i).(j) in
         CCVector.push avars.(c) v;
         for di = 0 to x-1 do
           for dj = 0 to y-1 do
-            if dj < Array.length (components.(c).(di)) 
-            && Option.is_some (components.(c).(di).(dj))
+            if dj < Array.length ((CCVector.get shapes c).(di)) 
+            && ((CCVector.get shapes c).(di).(dj))
             then
               CCVector.push at.(i+di).(j+dj) v
           done;
@@ -45,35 +65,90 @@ let layout_with_sizes n m (components : string option array array list)
       done;
     done;
   done;
-  try 
+  Format.printf "Variables: %d@." !nv;
+  try
+    let nclauses = ref 0 in
     let solver = Minisat.create () in
-    let distinct l =
+    (* Minisat.set_verbose solver 2; *)
+    let distinct_n2 l =
       for i = 0 to CCVector.length l - 1 do
         for j = 0 to i - 1 do
+          incr nclauses;
           let l1 = Minisat.Lit.(neg (make (CCVector.get l i))) in
           let l2 = Minisat.Lit.(neg (make (CCVector.get l j))) in
           Minisat.add_clause_l solver [ l1; l2 ]
         done;
       done
     in
-    let unique l =
-      distinct l;
-      Minisat.add_clause_l solver
-        (List.map (fun i -> Minisat.Lit.make i) (CCVector.to_list l));
+    let rec distinct vs =
+      if CCVector.length vs > 1 then begin
+        let ws = CCVector.create () in
+        CCVector.iter (fun i ->
+            if CCVector.is_empty ws
+            || CCVector.length (CCVector.get ws (CCVector.length ws - 1)) = 3
+            then CCVector.push ws (CCVector.create ());
+            CCVector.push (CCVector.get ws (CCVector.length ws - 1)) i
+          ) vs;
+        let nvs = CCVector.create () in
+        CCVector.iter (fun w ->
+            let vw = new_var () in
+            distinct_n2 w;
+            CCVector.iter (fun x ->
+                let l1 = Minisat.Lit.(neg (make x)) in
+                let l2 = Minisat.Lit.(make vw) in
+                Minisat.add_clause_l solver [ l1; l2 ]
+              ) w;
+            CCVector.push nvs vw
+          ) ws;
+        distinct nvs
+      end
+    in
+    let add_clause l =
+      let go l =
+        if l > 0
+        then Minisat.Lit.make l
+        else Minisat.Lit.(neg (make (-l)))
+      in
+      incr nclauses;
+      Minisat.add_clause_l solver (List.map go l)
+    in
+    let at_least count l =
+      let l = CCVector.to_array l in
+      if count > Array.length l then raise(Minisat.Unsat);
+      let sz = Array.length l in
+      let vars = Array.init (sz+1) (fun _ -> Array.init (count+1) (fun _ -> new_var ())) in
+      (* vars.(i).(j)   iff  the sum of l(1)..l(i-1) is j *)
+      for i = 0 to sz-1 do
+        for j = 0 to count - 1 do
+          add_clause [ - vars.(i).(j) ; - l.(i) ;  + vars.(i+1).(j+1) ];
+          add_clause [ - vars.(i).(j) ; - l.(i) ;  - vars.(i+1).(j) ];
+          add_clause [ - vars.(i).(j) ; + l.(i) ;  + vars.(i+1).(j) ];
+          add_clause [ - vars.(i).(j) ; + l.(i) ;  - vars.(i+1).(j+1) ];
+          add_clause [ - vars.(i+1).(j+1) ; vars.(i).(j) ; vars.(i).(j+1) ];
+          ()
+        done;
+      done;
+      add_clause [ vars.(0).(0) ];
+      for j = 1 to count do
+        add_clause [ - vars.(0).(j) ];
+      done;
+      add_clause [ vars.(sz).(count) ];
       ()
     in
-    for i = 0 to ncomponents-1 do
-      unique avars.(i)
+    for i = 0 to nshapes-1 do
+      at_least (CCVector.get shape_count i) avars.(i)
     done;
     for i = 0 to n-1 do
       for j = 0 to m-1 do
         distinct at.(i).(j)
       done;
     done;
+    Format.printf "Clauses: %d@." !nclauses;
+    Minisat.simplify solver;
     Minisat.solve solver;
     let out = Array.init n (fun _ -> Array.init m (fun _ -> None)) in
-    for c = 0 to ncomponents - 1 do
-      let (x,y) = sizes.(c) in
+    for c = 0 to nshapes - 1 do
+      let (x,y) = CCVector.get sizes c in
       for i = 0 to n-x do
         for j = 0 to m-y do
           let v = vars.(c).(i).(j) in
@@ -81,10 +156,10 @@ let layout_with_sizes n m (components : string option array array list)
             begin
               for di = 0 to x-1 do
                 for dj = 0 to y-1 do
-                  if dj < Array.length (components.(c).(di)) 
-                  && Option.is_some (components.(c).(di).(dj))
+                  if dj < Array.length ((CCVector.get shapes c).(di)) 
+                  && ((CCVector.get shapes c).(di).(dj))
                   then begin
-                    out.(i+di).(j+dj) <- components.(c).(di).(dj)
+                    out.(i+di).(j+dj) <- Some("x")
                   end
                 done;
               done;
@@ -96,6 +171,7 @@ let layout_with_sizes n m (components : string option array array list)
   with | Minisat.Unsat -> None
 
 let layout (components : string option array array list) : string option array array =
+  Format.printf "Layout@.";
   let exception Found of string option array array in
   let cx, cy =
     List.fold_left (fun (mx, my) c ->
@@ -103,15 +179,19 @@ let layout (components : string option array array list) : string option array a
         max mx mx', max my my')
       (0, 0) components
   in
+  Format.printf "(cx,cy) = (%d,%d)" cx cy;
   try 
-    for area = 1 to 1000000 do
+    for area = 1 to 100000 do
+      Format.printf "Trying area %d@." area;
       for n = cx to area do
         for m = cy to area do
           if area = n * m
-          then 
+          then begin
+            Format.printf "Trying %d = %d * %d@." area n m;
             match layout_with_sizes n m components with
             | Some(x) -> raise (Found(x))
             | None -> ()
+          end
         done;
       done;
     done;
--- bin/rafael_lambdaman.ml
+++ bin/rafael_lambdaman.ml
@@ -7,17 +7,8 @@ let () =
   (* let e = Miniterm.random_string seed in *)
   (* Format.printf "%a\n" Ast.print_ast Miniterm.(!~ "solve lambdaman7 " ^/ e); *)
 
-  let e = Miniterm.random_strings 62500
-      [154;288;292;97;1010;184;471;962;308;621;780;288;324;1007]
-  in
-  let e = Miniterm.lambdaman18 in
-  let term_s = Format.asprintf "%a" Ast.print_ast Miniterm.(!~ "solve lambdaman5 " ^/ e) in
-  let len = String.length term_s in
-  Format.eprintf "TERM@.%s@.@." term_s;
-  Format.eprintf "SIZE: %d@.@." len;
-  match Eval.eval Eval.EnvEmpty (Eval.term_from_expr e) with
-  | VBool _ | VInt _ | VLambda _ -> assert false
-  | VString(t) ->
+  let e = Miniterm.lambdaman12 in
+  let VString(t) = Eval.eval Eval.EnvEmpty (Eval.term_from_expr e) in
   let t = Rope.to_string t in
   let s = Ast.Encoded_string.to_string (Ast.Encoded_string.from_raw_string t) in
   Format.eprintf "%d@.@." (String.length s);
@@ -26,11 +17,11 @@ let () =
   (* let t = Eval.term_from_expr Miniterm.lambdaman19 in *)
   (* Format.printf "%a@." Ast.pp_expr Miniterm.lambdaman19; *)
   
-  let result = Api.communicate (Format.asprintf "%a" Ast.print_ast
-                                  Miniterm.(!~ "solve lambdaman11 " ^/ e)) in
-  let msg = Ast.parse_input result in
-  let r = Eval.eval EnvEmpty (Eval.term_from_expr msg) in
-  Format.printf "%a@." Eval.pp_value r;
+  (* let result = Api.communicate (Format.asprintf "%a" Ast.print_ast *)
+  (*                                 Miniterm.(!~ "solve lambdaman11 " ^/ e)) in *)
+  (* let msg = Ast.parse_input result in *)
+  (* let r = Eval.eval EnvEmpty (Eval.term_from_expr msg) in *)
+  (* Format.printf "%a@." Eval.pp_value r; *)
   
 
   ()
