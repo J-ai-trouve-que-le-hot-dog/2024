3c90ac1e929ab927fee50a5f61361f5d8797b023
Rafaël Bocquet <6364095+RafaelBocquet@users.noreply.github.com>
2024-07-01 07:46:39 +0200

    spaceship3 progress

--- rafael/spaceship3.cpp
+++ rafael/spaceship3.cpp
@@ -4,23 +4,21 @@ const i64 MAXN   = 100'000;
 const i64 OFFSET = 500'000;
 
 u64 hash_visited[MAXN];
-u64 hash_x[2*OFFSET+1];
-u64 hash_y[2*OFFSET+1];
+u64 hash_city[MAXN];
 u64 hash_vx[2*OFFSET+1];
 u64 hash_vy[2*OFFSET+1];
 
 void init_hash() {
   FOR(i, MAXN) hash_visited[i] = rng.randomInt64();
-  FOR(i, 2*OFFSET+1) hash_x[i] = rng.randomInt64();
-  FOR(i, 2*OFFSET+1) hash_y[i] = rng.randomInt64();
+  FOR(i, 2*OFFSET+1) hash_city[i] = rng.randomInt64();
   FOR(i, 2*OFFSET+1) hash_vx[i] = rng.randomInt64();
   FOR(i, 2*OFFSET+1) hash_vy[i] = rng.randomInt64();
 }
 
 vector<i64> neighbors[MAXN];
 
-const i64 MAXNEI = 32;
-const i64 MAXDV  = 8;
+const i64 MAXNEI = 8;
+const i64 MAXDV  = 1;
 
 struct beam_state {
   bool visited[MAXN];
@@ -31,7 +29,8 @@ struct beam_state {
   u64  hvisited;
 
   i64 time;
-  i64 history[MAXV];
+  i64 history[MAXN];
+  pt  history_speed[MAXN];
 
   void reset() {
     cost = 0;
@@ -42,17 +41,19 @@ struct beam_state {
     hvisited = 1; // TODO different seed for every iter
     time = 0;
     history[0] = 0;
+    history_speed[0] = speed;
   }
 
   bool do_move(problem const& pb, i64 i, i64 dvx, i64 dvy) {
     pt nspeed = speed + pt(dvx-MAXDV,dvy-MAXDV);
-    if(nspeed.x < -MAXV || nspeed.x > MAXV) return false;
-    if(nspeed.y < -MAXV || nspeed.y > MAXV) return false;
+    if(nspeed.x < 0 || nspeed.x > 2*MAXV) return false;
+    if(nspeed.y < 0 || nspeed.y > 2*MAXV) return false;
 
     cost += pb.cost(city, speed, i, nspeed);
     city = i;
     time += 1;
     history[time] = i;
+    history_speed[time] = nspeed;
     hvisited ^= hash_visited[i];
     speed = nspeed;
 
@@ -83,8 +84,24 @@ struct beam_state {
   
   u64 get_hash() const {
     return hvisited
+      ^ hash_city[city]
       ^ hash_vx[speed.x - MAXV + OFFSET]
       ^ hash_vy[speed.y - MAXV + OFFSET];
+  }
+
+  string reconstruct(problem const& pb) const {
+    string out;
+
+    i64 total_cost = 0;
+    
+    FOR(i, pb.n-1) {
+      auto a = history[i], b = history[i+1];
+      auto sa = history_speed[a], sb = history_speed[b];
+      pb.reconstruct(out,a,sa,b,sb);
+      total_cost += pb.cost(a,sa,b,sb);
+    }
+    debug(total_cost); // TODO
+    return out;
   }
 };
 
@@ -101,6 +118,7 @@ struct euler_tour {
     data[size++] = x;
   }
   FORCE_INLINE u32& operator[](i64 ix) { return data[ix]; }
+  FORCE_INLINE u32 const& operator[](i64 ix) const { return data[ix]; }
 };
   
 vector<euler_tour> tree_pool;
@@ -120,14 +138,52 @@ euler_tour get_new_tree(){
   return tour;
 }
 
-const i64 HASH_SIZE = 1ull<<30;
+const i64 HASH_SIZE = 1ull<25;
 const i64 HASH_MASK = HASH_SIZE-1;
 atomic<uint64_t> *HS = nullptr;
+atomic<int64_t> *HS_value = nullptr;
+
+void euler_tour_best_state
+    (beam_state &best_state,
+     problem const& pb,
+     i64 istep,
+     euler_tour const& tour_current)
+{
+  beam_state S; S.reset();
+
+  vector<i64> stack_moves(istep+1);
+  i64 nstack_moves = 0;
+
+  FOR(iedge, tour_current.size) {
+    auto const& edge = tour_current[iedge];
+    if(edge > 0) {
+      stack_moves[nstack_moves] = edge - 1;
+      S.do_move(pb, stack_moves[nstack_moves]);
+      nstack_moves += 1;
+    }else{
+      if(nstack_moves == istep) {
+        if(S.cost < best_state.cost) {
+          best_state = S;
+        }
+      }
+      
+      if(nstack_moves == 0) {
+        runtime_assert(S.cost == 0);
+        return;
+      }
+				
+      nstack_moves -= 1;
+      S.undo_move(pb,stack_moves[nstack_moves]);
+    }
+  }
+
+  runtime_assert(false);
+}
 
 void traverse_euler_tour
 (problem const& pb,
  i64 istep,
- euler_tour tour_current,
+ euler_tour const& tour_current,
  vector<euler_tour> &tours_next,
  vector<i64> &costs,
  i64 cutoff, float cutoff_keep_probability)
@@ -155,21 +211,6 @@ void traverse_euler_tour
             ncommit += 1;
           }
 
-//           if(S.nvisited > best) {
-// #pragma omp critical
-//             {
-//               if(S.nvisited > best) {
-//                 best = S.nvisited;
-//                 sol.clear();
-//                 FOR(i, nstack_moves) {
-//                   i64 m = stack_moves[i];
-//                   i64 dx = m/3, dy = m%3;
-//                   sol += dc[dy][dx];
-//                 }
-//               }
-//             }
-//           }
-
           i64 count = 0;
           FOR(ix, neighbors[S.city].size()) {
             i64 i = neighbors[S.city][ix];
@@ -180,7 +221,8 @@ void traverse_euler_tour
               if(S.do_move(pb,m)) {
                 auto h = S.get_hash();
                 auto prev = HS[h&HASH_MASK].exchange(h, std::memory_order_relaxed);
-                if(prev != h) {
+                if(prev != h || S.value() < HS_value[h&HASH_MASK]) {
+                  HS_value[h&HASH_MASK] = S.value();
                   costs.eb(S.value());
                   tour_next->push(1+m);
                   tour_next->push(0);
@@ -195,6 +237,7 @@ void traverse_euler_tour
       }
 
       if(nstack_moves == 0) {
+        runtime_assert(S.cost == 0);
         return;
       }
 
@@ -224,6 +267,8 @@ string beam_search(problem const& pb, i64 width) {
   if(!HS) {
     auto ptr = new atomic<uint64_t>[HASH_SIZE];
     HS = ptr;
+    auto ptr_value = new atomic<int64_t>[HASH_SIZE];
+    HS_value = ptr_value;
   }
   
   vector<euler_tour> tours_current;
@@ -281,8 +326,7 @@ string beam_search(problem const& pb, i64 width) {
     }
 
     if((i64)costs.size() > width) {
-      nth_element(begin(costs), begin(costs)+width, end(costs),
-                  greater<i64>());
+      nth_element(begin(costs), begin(costs)+width, end(costs));
       cutoff = costs[width];
       i64 nin = 0, ntotal = 0;
       FOR(i, costs.size()) if(costs[i] == cutoff) {
@@ -298,7 +342,7 @@ string beam_search(problem const& pb, i64 width) {
     for(auto const& t : tours_next) total_size += t.size;
     
     i64 low = *min_element(all(costs));
-    cerr << setw(3) << istep << ": " <<
+    cerr << setw(3) << istep+1 << "/" << pb.n-1 << ": " <<
       " costs = " << setw(3) << low << ".." << setw(3) << cutoff <<
       ", tree size = " << setw(12) << total_size <<
       ", num trees = " << setw(6) << tours_next.size() <<
@@ -308,6 +352,20 @@ string beam_search(problem const& pb, i64 width) {
     tours_current = tours_next;
   }
 
+  beam_state best_state; best_state.cost = 1e9;
+  for(auto tour : tours_current) {
+    euler_tour_best_state
+      (best_state,
+       pb,
+       pb.n-1,
+       tour
+       );
+  }
+
+  debug(best_state.cost);
+  auto sol = best_state.reconstruct(pb);
+  debug(sol.size());
+  
   return "";
 }
 
